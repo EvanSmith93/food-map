c3303e987ac686398f83ff80d0067399280645ef
EvanSmith93 <evan.smith993@gmail.com>
2025-04-14 16:41:23 -0600

    Add two recipes

--- src/data.ts
+++ src/data.ts
@@ -3,15 +3,24 @@ import { Pin } from "./WorldMap";
 export const pinData = [
   {
     id: "1",
-    position: [41.9028, 12.4964], // Rome, Italy
-    title: "Italy - Pasta",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+    position: [32.2986, 35.992251], // Middle East
+    title: "Hummus",
+    content: `1200s - Take chickpeas and pound them fine after boiling them. Then take
+vinegar, oil, tahineh, pepper, aṭrāf ṭīb, mint, parsley, the refuse of dry
+thyme, walnuts, hazelnuts, almonds, pistachios, Ceylon cinnamon,
+toasted caraway, dry coriander, salt, salted lemons and olives. Stir it
+and roll it out flat and leave it overnight and take it up `,
   },
   {
-    id: "2",
-    position: [35.6895, 139.6917], // Tokyo, Japan
-    title: "Japan - Rice",
+    id: "1",
+    position: [35.6895, 139.6917], // Japan
+    title: "Udon noodles",
+    content: `1643 - Beat the flour as much as is necessary. For salt seasoning, put 1 shō of salt in 3 shō of water in summer, and 1 shō of salt in 5 shō of water in winter. Using this saltwater, mix in to season just right. Affix the dough on a millstone, and carefully and without vibration roll it up to roughly ball shaped and put it in a box. Wet a cloth, cover, and put it in a place where the wind doesn’t blow. Take out one at a time and beat it. Boil and season to taste. The broth is ninuki, or possibly taremiso. Garnish with black pepper and ume. 1 shō ≈ 1/2 gallon.`,
+  },
+  {
+    id: "1",
+    position: [41.9028, 12.4964], // Rome, Italy
+    title: "Italy - Pasta",
     content:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
   },
