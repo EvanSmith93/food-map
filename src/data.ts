2b463ea9ba551a038e838191c55a14e84f1514b0
EvanSmith93 <evan.smith993@gmail.com>
2025-04-14 18:05:48 -0600

    Add a few more recipes

--- src/data.ts
+++ src/data.ts
@@ -5,72 +5,68 @@ export const pinData = [
     id: "1",
     position: [32.2986, 35.992251], // Middle East
     title: "Hummus",
-    content: `1200s - Take chickpeas and pound them fine after boiling them. Then take
-vinegar, oil, tahineh, pepper, aṭrāf ṭīb, mint, parsley, the refuse of dry
-thyme, walnuts, hazelnuts, almonds, pistachios, Ceylon cinnamon,
-toasted caraway, dry coriander, salt, salted lemons and olives. Stir it
-and roll it out flat and leave it overnight and take it up `,
+    content: `1200s - Take chickpeas and pound them fine after boiling them. Then take vinegar, oil, tahineh, pepper, aṭrāf ṭīb, mint, parsley, the refuse of dry thyme, walnuts, hazelnuts, almonds, pistachios, Ceylon cinnamon, toasted caraway, dry coriander, salt, salted lemons and olives. Stir it and roll it out flat and leave it overnight and take it up `,
   },
   {
-    id: "1",
+    id: "2",
     position: [35.6895, 139.6917], // Japan
     title: "Udon noodles",
     content: `1643 - Beat the flour as much as is necessary. For salt seasoning, put 1 shō of salt in 3 shō of water in summer, and 1 shō of salt in 5 shō of water in winter. Using this saltwater, mix in to season just right. Affix the dough on a millstone, and carefully and without vibration roll it up to roughly ball shaped and put it in a box. Wet a cloth, cover, and put it in a place where the wind doesn’t blow. Take out one at a time and beat it. Boil and season to taste. The broth is ninuki, or possibly taremiso. Garnish with black pepper and ume. 1 shō ≈ 1/2 gallon.`,
   },
   {
-    id: "1",
-    position: [41.9028, 12.4964], // Rome, Italy
-    title: "Italy - Pasta",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
-  {
     id: "3",
-    position: [20.5937, 78.9629], // India
-    title: "India - Rice and Roti",
+    position: [41.798333, 12.710833], // Italy
+    title: "Kneaded Bread",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+      "Wash your hands and a bowl thoroughly. Pour meal into the bowl, add water gradually, and knead thoroughly. When it is well kneaded, roll out and bake under a crock.",
   },
   {
     id: "4",
-    position: [23.6345, -102.5528], // Mexico
-    title: "Mexico - Corn",
+    position: [35.97653594, -83.923713929], // Cherokee Territory
+    title: "Kanuchi",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+      "Gather hickory nuts or scalybarks, dry on a rack before the fire. When the nuts are dry crack them by using a large flat rock placed in a flat basket lined temporarily with a cloth, use a smaller rock to pound the nuts when placed on the larger rock. When the nuts are all cracked sieve them through a sieve basket. Place the kernels and small hulls that passed through the sieve in the corn beater and pound until the substance can be made into balls. Roll this into balls until ready for use. These balls will keep fresh for several days if the weather is not too warm. When ready for Hickory Nut Soup place a ball or more in a vessel that will hold water, pour boiling water over the balls while stirring constantly. If this is made into a thick soup it may be served with any type bread or dumpling. If it is made into a thin soup it may be used as a drink. As soon as enough soup has been poured off to leave a very thick mixture more water may be added. Do not drink the very last of the mixture because that is where the little bits of hulls are.",
   },
-  {
-    id: "5",
-    position: [48.8566, 2.3522], // Paris, France
-    title: "France - Bread",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
-  {
-    id: "6",
-    position: [37.5665, 126.978], // Seoul, South Korea
-    title: "South Korea - Kimchi and Rice",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
-  {
-    id: "7",
-    position: [-14.235, -51.9253], // Brazil
-    title: "Brazil - Rice and Beans",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
-  {
-    id: "8",
-    position: [15.87, 100.9925], // Thailand
-    title: "Thailand - Rice and Noodles",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
-  {
-    id: "9",
-    position: [31.7917, -7.0926], // Morocco
-    title: "Morocco - Couscous",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
-  },
+  // {
+  //   id: "4",
+  //   position: [23.6345, -102.5528], // Mexico
+  //   title: "Mexico - Corn",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
+  // {
+  //   id: "5",
+  //   position: [48.8566, 2.3522], // Paris, France
+  //   title: "France - Bread",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
+  // {
+  //   id: "6",
+  //   position: [37.5665, 126.978], // Seoul, South Korea
+  //   title: "South Korea - Kimchi and Rice",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
+  // {
+  //   id: "7",
+  //   position: [-14.235, -51.9253], // Brazil
+  //   title: "Brazil - Rice and Beans",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
+  // {
+  //   id: "8",
+  //   position: [15.87, 100.9925], // Thailand
+  //   title: "Thailand - Rice and Noodles",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
+  // {
+  //   id: "9",
+  //   position: [31.7917, -7.0926], // Morocco
+  //   title: "Morocco - Couscous",
+  //   content:
+  //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.",
+  // },
 ] satisfies Pin[];
